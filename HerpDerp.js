4691a0dd4bbdc0fcd3a24260ccec61877f4acd5a
j200183 <41653018+j200183@users.noreply.github.com>
2018-07-24 15:46:39 -0700

    Create HerpDerp.js

---
+++ HerpDerp.js
@@ -0,0 +1,10 @@
+function enlarge(word){
+  word.style.fontSize= "30px";
+}
+
+function smaller(word){
+  word.style.fontSize="20px";
+}
+function press_button(word){
+  word.innerHTML = "It's ami Mario!";
+}
