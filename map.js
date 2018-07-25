0f17fe3e5abf03d1c32c3df4828c8af7eec52b3f
j200183 <41653018+j200183@users.noreply.github.com>
2018-07-25 15:29:00 -0700

    Create map.js

---
+++ map.js
@@ -0,0 +1,117 @@
+//Create setup map variables
+var outLoc;
+var view;
+var map;
+
+function init(){
+  ourLoc = ol.proj.fromLonLat([-122.1468,47.6820]);
+
+  view = new ol.View({
+    center: ourLoc,
+    zoom: 6
+  });
+  map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    loadTilesWhileAnimating: true,
+    view: view
+  });
+}
+
+function panHome(){
+  view.animate({
+    center: ourLoc,
+    duration:2000
+  });
+}
+/*function panToLocation(){
+
+  var countryName = document.getElementById("country-name").value;
+  var query = "https://restcountries.eu/rest/v2/name/"+countryName;
+  query = query.replace(/ /g, "%20")
+  var countryRequest = new XMLHttpRequest();
+  countryRequest.open('GET',query,false)
+  countryRequest.send();
+  if(countryName === ""){
+    alert("You didn't enter a country name!");
+    return;
+  }
+  if (countryRequest.readyState != 4 || countryRequest.status != 200 || countryRequest.responseText === ""){
+    window.console.error("Request had an error");
+    return;
+  }
+  var countryInformation= JSON.parse(countryRequest.responseText);
+  var lon = countryInformation[0].latlng[0];
+  var lat = countryInformation [0].latlng[0];
+  var location = ol.proj.fromLonLat([lon,lat]);
+
+  view.animate({
+    center: location,
+    duration: 2000
+  });
+}*/
+function panToCountry() {
+	var countryName = document.getElementById("country-name").value;
+
+	if(countryName === "") {
+	 	alert("You didn't enter a country name!");
+	 	return;
+	}
+
+	var query = "https://restcountries.eu/rest/v2/name/"+countryName+"?fullText=true"
+
+	query = query.replace(/ /g, "%20")
+
+	var countryRequest = new XMLHttpRequest();
+	countryRequest.open('GET', query, false);
+
+	countryRequest.send();
+
+	//window.console.log("Ready State " + countryRequest.readyState);
+	//window.console.log("Status " + countryRequest.status);
+	//window.console.log("Response" + countryRequest.responseText);
+
+	// Step 1: First we should only pan if the information was correct:
+	if(countryRequest.readyState != 4 || countryRequest.status != 200 || countryRequest.responseText === "") {
+	 	window.console.error("Request had an error!");
+	 	return;
+	}
+
+	// Step 2: Let's copy this output into a text file and
+	// see where the latitude and longitude live
+	// We need to convert this to JSON using the JSON.parse
+	// function in order to use the data.
+	//window.console.log(countryRequest.responseText);
+	var countryInformation = JSON.parse(countryRequest.responseText);
+
+	// Step 3: We have to figure out where the information is based
+	// on the JSON we got back. This can be very tricky sometimes.
+	// For instance, this JSON returns an ARRAY of information.
+	// Inside the FIRST array element, we have our latlng variable.
+	// This variable has the information we need!
+	var lat = countryInformation[0].latlng[0];
+	var lon = countryInformation[0].latlng[1];
+
+	// Note: If you run into an error like the map
+	// disappearing, check that you have your
+	// longtidue and latitude variables mapped
+	// to the right indexes. Lon is index 1,
+	// lat is index 0.
+	window.console.log(countryName + ": lon " + lon + " & lat " + lat);
+
+	var location = ol.proj.fromLonLat([lon, lat]);
+
+	// Note: If you run into an error like window
+	// not loading, check that you declared VAR
+	// before the location variable.
+
+	view.animate({
+		center: location, // Location
+		duration: 2000  // Two seconds
+	});
+}
+window.onload = init;
