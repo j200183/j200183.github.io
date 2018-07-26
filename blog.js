c15301a320143a55ce9e943d98cfd0908d3d428d
j200183 <41653018+j200183@users.noreply.github.com>
2018-07-26 13:54:19 -0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -6,5 +6,28 @@ function smaller(word){
   word.style.fontSize="20px";
 }
 function press_button(word){
-  word.innerHTML = "It's ami Mario!";
+  word.style.display='block';
 }
+function hide_button(word){
+  word.style.display='none';
+}
+function navchange(x){
+  x.style.backgroundColor="#cccccc";
+  x.style.color="white";
+}
+function nav_restore(x){
+  x.style.backgroundColor="white";
+  x.style.color="black";
+}
+function waitnav(x){
+  setTimeout(navchange(x),1000);
+}
+function menuhover(x,y){
+  x.style.display="block";
+  y.style.backgroundColor="#cccccc";
+}
+function menuout(x,y){
+  x.style.display="none";
+  y.style.backgroundColor="white";
+}
+
